670350364fb52b2a88fd2c7e922c6f55baa0920c
LeninPA <lenin.pavon@comunidad.unam.mx>
2020-06-21 19:29:53 -0500

    FEAT: Primera capa de validación desde servidor

--- dynamics/php/Registro.php
+++ dynamics/php/Registro.php
@@ -10,27 +10,33 @@
     exit();
   }
   else {
-    $nombre = $_POST['nombre'];
-    $paterno = $_POST['paterno'];
-    $materno = $_POST['materno'];
-    $fecha = $_POST['fecha'];
-    $correo = $_POST['correo'];
-    $id1 = $_POST['id1'];
-    $id2 = $_POST['id2'];
-    $password = $_POST['password'];
-    $tipo = $_POST['tipo'];
+    $nombre = (isset($_POST['nombre']) && $_POST['nombre'] != "") ? $_POST['nombre'] : false ;
+    $paterno = (isset($_POST['paterno']) && $_POST['paterno'] != "") ? $_POST['paterno'] : false ;
+    $materno = (isset($_POST['materno']) && $_POST['materno'] != "") ? $_POST['materno'] : false ;
+    $fecha = (isset($_POST['fecha']) && $_POST['fecha'] != "") ? $_POST['fecha'] : false ;
+    $correo = (isset($_POST['correo']) && $_POST['correo'] != "") ? $_POST['correo'] : false ;
+    $id1 = (isset($_POST['id1']) && $_POST['id1'] != "") ? $_POST['id1'] : false ;
+    $id2 = (isset($_POST['id2']) && $_POST['id2'] != "") ? $_POST['id2'] : false ;
+    $password = (isset($_POST['password']) && $_POST['password'] != "") ? $_POST['password'] : false ;
+    $tipo = (isset($_POST['tipo']) && $_POST['tipo'] != "") ? $_POST['tipo'] : false ;
   
-    if($tipo == "alumno"){
-      $tipo = 1;
-    }elseif ($tipo == "profesor") {
-      $tipo = 2;
-    }else{
-      $tipo = 3;
+    if($nombre === false || $paterno === false || $materno === false || $fecha === false || $correo === false || $id1 === false || $id2 === false || $password === false || $tipo === false ){
+      echo "Error al recibir sus datos, por favor reingréselos";
+    }
+    else{
+      if($tipo == "alumno"){
+        $tipo = 1;
+      }elseif ($tipo == "profesor") {
+        $tipo = 2;
+      }else{
+        $tipo = 3;
+      }
+  
+      $SQL_usr = "INSERT INTO usuario(id_usuario, id_tipo, password, nacimiento, correo, extra, nombre, paterno, materno) VALUES ('$id1', $tipo, '$password', '$fecha', '$correo', '$id2', '$nombre', '$paterno', '$materno')";
+      $query_usr = mysqli_query($conexion,$SQL_usr);
+    
+      echo $SQL_usr;
     }
 
-    $SQL_usr = "INSERT INTO usuario(id_usuario, id_tipo, password, nacimiento, correo, extra, nombre, paterno, materno) VALUES ('$id1', $tipo, '$password', '$fecha', '$correo', '$id2', '$nombre', '$paterno', '$materno')";
-    $query_usr = mysqli_query($conexion,$SQL_usr);
-  
-    echo $SQL_usr;
   }
 ?>
