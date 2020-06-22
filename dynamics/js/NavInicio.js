d26b5b44743d3129e770ff7bd441c7cab00e08b6
MelitoMT <marianatellez2003@comunidad.unam.mx>
2020-06-22 09:28:32 -0500

    FEAT:Ciclo for para cartas feed principal

--- dynamics/js/NavInicio.js
+++ dynamics/js/NavInicio.js
@@ -15,6 +15,26 @@ function ask(file){
         console.log(error.mesage)
     })
 }
+/* Genera feed principal */
+function askInicio(file){
+    fetch(file)
+    .then((result)=>{
+        return result.text();
+    })
+    .then((convert)=>{
+        content.innerHTML = convert;
+        let cardFeed = document.querySelector("#cardFeed");
+        for(let i=0;i<9;i++){
+            let card = document.createElement("div");
+            card.classList.add("card")
+            let link = document.createElement("a");
+            link.setAttribute("href","#");
+            link.classList.add("link");
+            card.appendChild(link);
+            cardFeed.appendChild(card);
+        }
+    })
+}
 fetch('Inicio.html')
     .then((response)=>{
         return response.text();
@@ -25,7 +45,7 @@ fetch('Inicio.html')
             ask('Crear.html');
         })
         inicio.addEventListener("click",()=>{
-            ask('Inicio.html');
+            askInicio('Inicio.html');
         })
         perfil.addEventListener("click",()=>{
             ask('Perfil.html');
