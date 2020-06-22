dc43ce19c74eec6792eb25bb1c6e4957b525db6e
Carlos-V24 <vihcprepa6@comunidad.unam.mx>
2020-06-21 20:19:00 -0500

    Se añadio el generador de preguntas y opciones de respuesta en Crear_formularios

---
+++ dynamics/js/Crear_formulario.js
@@ -0,0 +1,52 @@
+function añadirPreg() {
+  //Se crea el contenedor de la nueva pregunta
+  let newPreg = $("<div>")
+  newPreg.addClass("Pregunta")
+  let numPreg = ($(".Pregunta").length+1);//Se calcula su numero de pregunta
+  newPreg.attr('id', ("P-"+numPreg));//Se agrega su id
+  newPreg.append("<h2>Pregunta "+numPreg+"</h2>")
+  newPreg.append("<input type='text' name='P-"+numPreg+"' placeholder='Ingrese su pregunta' required>")//Su pregunta
+  newPreg.append("<h3>Opciones de respuesta</h3>")
+  ////**  Opciones de respuesta  **////
+  //Boton para añadir una opcion extra
+  let btnAñadirOpc = $("<button type='button' class='Añadir_Opc'>Añadir Opcion</button>")
+  btnAñadirOpc.click(()=>{
+    opciones.append(añadirResp(numPreg))
+  });
+  //Contenedor de las opciones
+  let opciones = $("<div>");
+  opciones.addClass("Opciones")
+  opciones.append(btnAñadirOpc)
+  newPreg.append(opciones)
+  $("body").append(newPreg)
+  //Se añade las dos primeras respuestas
+  for (var i = 0; i < 2; i++) {
+    console.log(numPreg);
+    opciones.append(añadirResp(numPreg ))
+  }
+}
+function añadirResp(numPregunta) {
+  /*Aqui se calcula cuantos hijos (respuestas) hay en las opciones
+    El valor de numero de pregunta es igual A sus hijos(total adentro)
+    -1(El boton de añadir)+1(La respuesta actual) por eso lo siguiente*/
+  let numOpc = ($("#P-"+numPregunta).children()[3].children.length);
+  //Se crea el p donde se guarda esto
+  let newOpc = $("<p>")
+  newOpc.append("<input type='text' placeholder='Ingrese su respuesta' required>")
+  //Si hay mas de dos opciones en esa pregunta puedes eliminar esa opcion
+  if (numOpc>2) {
+    //Boton para eliminar esa opcion
+    let btnElimOpc = $("<button type='button' class='Elim_Opc'>Eliminar</button>")
+    btnElimOpc.click(()=>{
+      newOpc.empty()
+    });
+    newOpc.append(btnElimOpc)
+  }
+  return newOpc
+}
+//Boton para añadir preguntas
+$("#Anadir_preg").click(()=>{
+  añadirPreg();
+})
+//Añade la primera pregunta
+añadirPreg();
