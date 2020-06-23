e88e0e6077bf3c50cc7e8308ac0605abe91a68a8
LeninPA <lenin.pavon@comunidad.unam.mx>
2020-06-22 22:40:30 -0500

    REFACTOR: Se organizan fetch en métodos

--- dynamics/js/Crear_formulario.js
+++ dynamics/js/Crear_formulario.js
@@ -54,14 +54,6 @@ class Pregunta{
 }
 // Creación del formulario
 class Formulario {
-  crearId(){
-    //Generador de id Para el formulario
-    let simbolos = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-    var id_form = "";
-    for (var i = 0; i < 6; i++) {
-      id_form += simbolos.substr(Math.round(Math.random() * simbolos.length), 1);
-    }
-  }
   constructor() {
     // Se añade el evento input (actualiza conforme se ingresa el valor del título)
     this.titulo = "Formulario";
@@ -155,11 +147,87 @@ class Formulario {
   modForm() {
     console.log("Do se puede")
   }
+
+  ////////*** Configuracion del formulario ***////////
+  //Se agregan los rangos de disponibilidad
+  addRangos(){
+    fetch('../dynamics/php/getRangos.php')
+      .then((response) => {
+        return response.json();
+      }).then((data) => {
+        let select = document.getElementById('rango');
+        data.forEach(element => {
+          console.log(element);
+          let new_option = document.createElement("option");
+          new_option.value = element.id_rango;
+          new_option.innerText = element.rango;
+          select.appendChild(new_option);
+        });
+        document.getElementById('Form_config').appendChild(select)
+      });
+  }
+  //Se agregan las Categorias
+  addCategorias(){
+    fetch('../dynamics/php/getCategorias.php')
+    .then((response) => {
+      return response.json();
+    }).then((data) => {
+      let select = document.getElementById('categoria');
+      data.forEach(element => {
+        let new_option = document.createElement("option");
+        new_option.value = element.id_categoria;
+        new_option.innerText = element.categoria;
+        select.appendChild(new_option);
+      });
+      document.getElementById('Form_config').appendChild(select)
+    });
+  }
+  addDate () {
+    var actual = new Date();
+    //Se agrego el input date de Inicio
+    $('#Form_config').append("Inicio: ")
+    let fechaInicio = $("<input type='date' name='Inicio_dia' required>")
+    var diaInicio = actual.getDate();
+    if (diaInicio<10) {
+      diaInicio = "0"+diaInicio;
+    }
+    var mesInicio = actual.getMonth()
+    if (mesInicio<10) {
+      mesInicio = "0"+mesInicio;
+    }
+    var añoInicio = actual.getFullYear();
+    fechaInicio.val(añoInicio+"-"+mesInicio+"-"+diaInicio)
+    console.log(añoInicio+"-"+mesInicio+"-"+diaInicio);
+    $('#Form_config').append(fechaInicio)
+    
+    //Se agrego el input tyme de Inicio
+    let tiempoInicio = $("<input type='time' name='Inicio_hora' required>")
+    var horaInicio = actual.getHours();
+    if (horaInicio<10) {
+      horaInicio= "0"+horaInicio
+    }
+    var minutosInicio = actual.getMinutes()
+    if (minutosInicio<10) {
+      minutosInicio= "0"+minutosInicio
+    }
+    console.log(horaInicio+":"+minutosInicio);
+    tiempoInicio.val(horaInicio+":"+minutosInicio)
+    $('#Form_config').append(tiempoInicio)
+    
+    //Dia y Hora de cierre de formulario
+    $('#Form_config').append("Fin: ")
+    $('#Form_config').append("<input type='date' name='Fin_dia' required>")
+    $('#Form_config').append(("<input type='time' name='Fin_hora' required>"))
+  }
 }
 
 // Instancia formulario
 let form = new Formulario();
+// Creación de configuración
 form.addPreg();
+form.addRangos();
+form.addCategorias();
+form.addDate();
 //Boton para añadir preguntas
 $("#Anadir_preg").click(()=>{
   form.addPreg();
@@ -176,104 +244,11 @@ $("#Eliminar_preg").click(()=>{
   }
 })
 
-////////*** Configuracion del formulario ***////////
-
-//Se agregan los rangos de disponibilidad
-fetch('../dynamics/php/getRangos.php')
-  .then((response) => {
-    return response.json();
-  }).then((data) => {
-    let select = document.getElementById('rango');
-    data.forEach(element => {
-      console.log(element);
-      let new_option = document.createElement("option");
-      new_option.value = element.id_rango;
-      new_option.innerText = element.rango;
-      select.appendChild(new_option);
-    });
-    document.getElementById('Form_config').appendChild(select)
-  });
-//Se agregan las Categorias
-fetch('../dynamics/php/getCategorias.php')
-  .then((response) => {
-    return response.json();
-  }).then((data) => {
-    let select = document.getElementById('categoria');
-    data.forEach(element => {
-      let new_option = document.createElement("option");
-      new_option.value = element.id_categoria;
-      new_option.innerText = element.categoria;
-      select.appendChild(new_option);
-    });
-    document.getElementById('Form_config').appendChild(select)
-  });
 
 
-
-var actual = new Date();
-//Se agrego el input date de Inicio
-$('#Form_config').append("Inicio: ")
-let fechaInicio = $("<input type='date' name='Inicio_dia' required>")
-var diaInicio = actual.getDate();
-if (diaInicio<10) {
-  diaInicio = "0"+diaInicio;
-}
-var mesInicio = actual.getMonth()
-if (mesInicio<10) {
-  mesInicio = "0"+mesInicio;
-}
-var añoInicio = actual.getFullYear();
-fechaInicio.val(añoInicio+"-"+mesInicio+"-"+diaInicio)
-console.log(añoInicio+"-"+mesInicio+"-"+diaInicio);
-$('#Form_config').append(fechaInicio)
-
-//Se agrego el input tyme de Inicio
-let tiempoInicio = $("<input type='time' name='Inicio_hora' required>")
-var horaInicio = actual.getHours();
-if (horaInicio<10) {
-  horaInicio= "0"+horaInicio
-}
-var minutosInicio = actual.getMinutes()
-if (minutosInicio<10) {
-  minutosInicio= "0"+minutosInicio
-}
-console.log(horaInicio+":"+minutosInicio);
-tiempoInicio.val(horaInicio+":"+minutosInicio)
-$('#Form_config').append(tiempoInicio)
-
-//Dia y Hora de cierre de formulario
-$('#Form_config').append("Fin: ")
-$('#Form_config').append("<input type='date' name='Fin_dia' requiered>")
-$('#Form_config').append(("<input type='time' name='Fin_hora' required>"))
-
-//Subir el formulario
 var btnCrear = $("<button type='button' name='button' id='Crear_form'>Crear Formulario</button>")
 btnCrear.click(()=>{
-  //Generador de id Para el formulario
-  let simbolos ="ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-  var id_form = ""
-  for (var i = 0; i < 6; i++) {
-    id_form+=simbolos.substr(Math.round(Math.random()*simbolos.length), 1);
-  }
-  //Insercion de datos del formulario
-  console.log("id_form = "+id_form);
-  console.log("Titulo = "+ $("input[name='Titulo']").val());
-  console.log("reportes = "+ 0);
-  for (var i = 0; i < form.cPreguntas; i++) {
-    //Se crea una pregunta
-    let id_preg = id_form+"-P"+(i+1)
-    let tituloPreg = $("input[name='P-"+(i+1)+"']").val()
-    //"INSERT INTO pregunta (id_pregunta, id_form, titulo) VALUES ('id_preg','id_form', 'tituloPreg')"
-    for (var n = 0; n < form.preguntas[i].cOpciones; n++) {
-      //Aqui se crean las opciones de esa pregunta
-      //Se crea el id_de la opcion
-      let id_opc = id_preg+"-"+(n+1)
-      //Obtiene el valor del input de esa opcion
-      let valorOpc = $("#P-"+(i+1))[0].children[3].children[n+1].getElementsByTagName("input")[0].value
-      //"INSERT INTO opcion (id_opcion, id_pregunta, valor) VALUES ('id_opc','id_preg', 'valorOpc')"
-      console.log("Pregunta "+id_preg+", opcion:"+(n+1));
-    }
-  }
+  console.log("Yamete kudasai")
 })
 $('#Form_config').append("<br><br>")
 $('#Form_config').append(btnCrear)
