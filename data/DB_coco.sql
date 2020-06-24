9d9a815479ecfe60e9af5bd9b3941703a6d3919f
LeninPA <lenin.pavon@comunidad.unam.mx>
2020-06-23 20:23:33 -0500

    FEAT: Base de datos

--- data/DB_coco.sql
+++ data/DB_coco.sql
@@ -84,6 +84,8 @@ CREATE TABLE `formulario` (
   `reportes` smallint(13) DEFAULT '0',
   `inicio_hora` time DEFAULT NULL,
   `fin_hora` time DEFAULT NULL,
+  `descripcion` text NOT NULL,
+  `id_usuario` varchar(13) NOT NULL,
   PRIMARY KEY (`id_form`),
   KEY `FK_id_categoria` (`id_categoria`),
   KEY `FK_rango` (`rango`),
@@ -98,6 +100,7 @@ CREATE TABLE `formulario` (
 
 LOCK TABLES `formulario` WRITE;
 /*!40000 ALTER TABLE `formulario` DISABLE KEYS */;
+INSERT INTO `formulario` VALUES ('BB946N',1,'Formulario',NULL,NULL,1,0,NULL,NULL,'',''),('uwu',1,'uwu Consultas',NULL,NULL,1,0,NULL,NULL,'','');
 /*!40000 ALTER TABLE `formulario` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -274,4 +277,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-06-23 13:18:48
+-- Dump completed on 2020-06-23 19:39:27
