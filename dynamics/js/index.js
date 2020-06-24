439e224be36c2aab47757d067e6eb49692ed3dc6
Carlos-V24 <vihcprepa6@comunidad.unam.mx>
2020-06-23 19:08:20 -0500

    FIX: Se arreglo las rutas relativas

--- dynamics/js/index.js
+++ dynamics/js/index.js
@@ -18,7 +18,8 @@ var count = 1;
 };
 function acceso() {
     let data = new FormData(document.getElementById('formAcceso'));
-    fetch('../dynamics/php/Acceso.php', {
+    console.log(window.location);
+    fetch('./dynamics/php/Acceso.php', {
       method: 'POST',
       body: data
     }).then((response) => {
@@ -36,20 +37,21 @@ function acceso() {
   }
     function registro() {
         let data = new FormData(document.getElementById('miForm'));
-        fetch('../dynamics/php/Registro.php', {
+        fetch('./dynamics/php/Registro.php', {
             method: 'POST',
             body: data
             })
             .then((response) => {
             return response.text();
             }).then((data) => {
+              console.log("Todoc cool");
             alert(data);
             document.getElementById('miForm').reset();
             }).catch((message)=>{
             alert(message);
             });
     }
-    
+
 let formIngreso = document.querySelector("#formIngreso");
 let formRegistro = document.querySelector("#formRegistro");
 let divRegistro = document.querySelector("#publicForm");
@@ -107,26 +109,26 @@ formRegistro.addEventListener("click",()=>{
             id2.maxLength = "6"
             }
         //Validador de contraseña segura
-        password.addEventListener("blur",()=>{
-        let mensajePsw = document.createElement("p")
-        mensajePsw.classList.add("Error_psw")
-        //El mensaje de error ya creado
-        if (document.querySelector("p.Error_psw")!=null) {
-            //Cambien el regex como crean conveniente
-            if (password.value.search(/^(?=[\w!#$@%&*^+-]*\d)(?=[\w!#$@%&*^+-]*[A-Z])(?=[\w!#@$%&*^+-]*[a-z])(?=[\w!#$%&*^+@-]*[!#$%&*@^+-])\S{8,100}$/)>=0) {
-            document.querySelector("p.Error_psw").innerText = "Contraseña segura";
-            }else {
-            document.querySelector("p.Error_psw").innerText = "Contraseña insegura";
-            }
-        }else {
-            if (!(password.value.search(/^(?=[\w!#$@%&*^+-]*\d)(?=[\w!#$@%&*^+-]*[A-Z])(?=[\w!#@$%&*^+-]*[a-z])(?=[\w!#$%&*^+@-]*[!#$%&*@^+-])\S{8,100}$/)>=0)) {
-            mensajePsw.innerText = "Contraseña insegura";
-            document.getElementsByTagName("form")[0].insertBefore(mensajePsw, tipo)
-           
-}
-        }
-
-        })
+//         password.addEventListener("blur",()=>{
+//         let mensajePsw = document.createElement("p")
+//         mensajePsw.classList.add("Error_psw")
+//         //El mensaje de error ya creado
+//         if (document.querySelector("p.Error_psw")!=null) {
+//             //Cambien el regex como crean conveniente
+//             if (password.value.search(/^(?=[\w!#$@%&*^+-]*\d)(?=[\w!#$@%&*^+-]*[A-Z])(?=[\w!#@$%&*^+-]*[a-z])(?=[\w!#$%&*^+@-]*[!#$%&*@^+-])\S{8,100}$/)>=0) {
+//             document.querySelector("p.Error_psw").innerText = "Contraseña segura";
+//             }else {
+//             document.querySelector("p.Error_psw").innerText = "Contraseña insegura";
+//             }
+//         }else {
+//             if (!(password.value.search(/^(?=[\w!#$@%&*^+-]*\d)(?=[\w!#$@%&*^+-]*[A-Z])(?=[\w!#@$%&*^+-]*[a-z])(?=[\w!#$%&*^+@-]*[!#$%&*@^+-])\S{8,100}$/)>=0)) {
+//             mensajePsw.innerText = "Contraseña insegura";
+//             document.getElementsByTagName("form")[0].insertBefore(mensajePsw, tipo)
+//
+// }
+//         }
+//
+//         })
         //---------Registro---------//
         /////Añadimos un manejador de eventos para cuando el formulario se envíe/////
         document.getElementById('miForm').addEventListener('submit', (e) => {
@@ -149,5 +151,5 @@ formIngreso.addEventListener("click",()=>{
             e.preventDefault();
             acceso();
           })
-        })        
+        })
     })
