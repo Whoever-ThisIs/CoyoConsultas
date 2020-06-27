5c592b24890842f5a0ea2ab00b5fe8d06286d22d
LeninPA <lenin.pavon@comunidad.unam.mx>
2020-06-26 21:09:17 -0500

    FEAT: Verificación de usr para responder formulario

--- dynamics/js/Responder_formulario.js
+++ dynamics/js/Responder_formulario.js
@@ -1,3 +1,9 @@
+/**Este programa verifica si un usuario puede
+ * responder una encuesta y le permite contestarla y guarda sus
+ * resultados si cuenta con la autorización, o lo devuelve a la
+ * página de inicio
+ */
+// Función para obtener el valor de una cookie
 function getCookie(name) {
   var re = new RegExp(name + "=([^;]+)");
   var value = re.exec(document.cookie);
@@ -6,69 +12,86 @@ function getCookie(name) {
 ////////////////////////////////////////////////////////////////////////////////
 //////////////////////  Crear el formulario de respuesta  //////////////////////
 ////////////////////////////////////////////////////////////////////////////////
+// Se obtiene el valor del formulario
 var id_formu = getCookie("id_form");
+// Verifica que existe
 if(id_formu != false){
-  //Se crea el id_form para obtenerlo luego por metodo POST en el php
+  // Se crea el id_form para obtenerlo luego por metodo POST en el php
   getForm = new FormData();
-  getForm.append("id_form", id_formu);//<== Ingrese aui el id del formulario
-  //Se manda la peticion
-  fetch('../dynamics/php/Resp_form.php', {
+  getForm.append("id_form", id_formu);
+  // Se obtiene la autorización del usuario
+  fetch('../dynamics/php/Verif_usr.php', {
     method: 'POST',
     body: getForm
   })
-  .then((response) => {
-    // Se decodifica el resultado de JSON y genera un objeto
-    return response.json();
-  }).then((data) => {
-    // Guarado ese objeto como "Formulario"
-    var formulario = data;
-    // Pongo en la página su nombre y descripcion
-    $("#Nom_form").text(formulario.Titulo)
-    $("#Desc_form").text(formulario.Desc)
-    // Guardo variable de las preguntas de Formulario
-    var preguntas = formulario[0].Preguntas;
-    // Imprimo todas las preguntas
-    for (var n = 0; n < preguntas.length; n++) {
-      // Imprimo el nombre de la pregunta
-      $("#Preguntas").append("<h3>Pregunta "+(n+1)+": "+preguntas[n].Titulo+"</h3>");
-      //Guardo el id de esa pregunta, sus opciones
-      var id_preg = preguntas[n].id_preg;
-      var opciones = formulario[0].Preguntas[n].Opciones;
-      //Creo el contenedor donde van a estar las opciones
-      var divOpc = $("<div>")
-      //Imprimo todas las opciones de cada pregunta en input radio
-      for (var i = 0; i < opciones.length; i++) {
-        var opc = $("<input type='radio' id='"+opciones[i].id_opc+"' name='"+id_preg+"' value='"+i+"'>");
-        //Si es la primera pregunta la preseleciona
-        if (i==0) {
-          opc.prop('checked', true)
+  .then(respuesta => respuesta.text())
+  .then((bool_usr) => {
+    // Si tiene la autorización, se muestra
+    if(bool_usr == 1){
+      // Se pide el formulario
+      fetch('../dynamics/php/Resp_form.php', {
+        method: 'POST',
+        body: getForm
+      })
+      .then((response) => {
+        // Se decodifica el resultado de JSON y genera un objeto
+        return response.json();
+      }).then((data) => {
+        // Se guarda el objeto como "Formulario"
+        var formulario = data;
+        // Se pone en la página su nombre y descripcion
+        $("#Nom_form").text(formulario.Titulo)
+        $("#Desc_form").text(formulario.Desc)
+        // Guardo variable de las preguntas de Formulario
+        var preguntas = formulario[0].Preguntas;
+        // Se imprimen las preguntas
+        for (var n = 0; n < preguntas.length; n++) {
+          // Se imprime el nombre de la pregunta
+          $("#Preguntas").append("<h3>Pregunta "+(n+1)+": "+preguntas[n].Titulo+"</h3>");
+          // Se guarda el id de esa pregunta, sus opciones
+          var id_preg = preguntas[n].id_preg;
+          var opciones = formulario[0].Preguntas[n].Opciones;
+          // Se crea el contenedor donde van a estar las opciones
+          var divOpc = $("<div>")
+          // Imprimo todas las opciones de cada pregunta en input radio
+          for (var i = 0; i < opciones.length; i++) {
+            var opc = $("<input type='radio' id='"+opciones[i].id_opc+"' name='"+id_preg+"' value='"+i+"'>");
+            // Si es la primera opcion la preseleciona
+            if (i==0) {
+              opc.prop('checked', true)
+            }
+            divOpc.append(opc)
+            // La respuesta (el texto que el usuario ve) de la pregunta
+            divOpc.append("<label for='"+opciones[i].id_opc+"'>"+opciones[i].valor+"</label><br>")
+          }
+          // Se imprime esa pregunta
+          $("#Preguntas").append(divOpc)
         }
-        divOpc.append(opc)
-        // La respuesta (el texto que el usuario ve) de la pregunta
-        divOpc.append("<label for='"+opciones[i].id_opc+"'>"+opciones[i].valor+"</label><br>")
-      }
-      //Imprimo esa pregunta
-      $("#Preguntas").append(divOpc)
+      });
+      ////////////////////////////////////////////////////////////////////////////////
+      //////////////////////  Subir el formulario de respuesta  //////////////////////
+      ////////////////////////////////////////////////////////////////////////////////
+      // Evento de subir la info
+      $("#Enviar").click(()=>{
+        //Se guarda los resultados de los radio
+        sendForm = new FormData(document.getElementById('Formu'));
+        sendForm.append("id_form", id_formu);//<== Ingrese aui el id del formulario
+        // Se hace la peticion
+        fetch('../dynamics/php/Respuesta_form.php', {
+          method: 'POST',
+          body: sendForm
+        }).then((response) => {
+          return response.text();
+        }).then((text) => {
+          console.log(text);
+        })
+      })
+      // Sin autorización se redirige a inicio
+    }else{
+      window.location = "./Inicio.html";
     }
-  });
-  ////////////////////////////////////////////////////////////////////////////////
-  //////////////////////  Subir el formulario de respuesta  //////////////////////
-  ////////////////////////////////////////////////////////////////////////////////
-  //Evento de subir la info
-  $("#Enviar").click(()=>{
-    //Se guarda los resultados de los radio
-    sendForm = new FormData(document.getElementById('Formu'));
-    sendForm.append("id_form", id_formu);//<== Ingrese aui el id del formulario
-    //Se hace la peticion
-    fetch('../dynamics/php/Respuesta_form.php', {
-      method: 'POST',
-      body: sendForm
-    }).then((response) => {
-      return response.text();
-    }).then((text) => {
-      console.log(text);
-    })
   })
 }else{
+  // Si no existe el formulario se redirige a inicio
   window.location = "./Inicio.html";
 }
---
+++ dynamics/php/Verif_usr.php
@@ -0,0 +1,42 @@
+<?php
+  include("Config.php");
+  $con = connect();
+  session_start();
+  $id_form = isset($_POST['id_form']) ? $_POST['id_form'] : "";
+  $id_usr = isset($_SESSION['id']) ? $_SESSION['id'] : "";
+
+  $SQL_tipo_usr = "SELECT tipo_usr FROM tipo WHERE id_tipo IN(SELECT id_tipo FROM usuario WHERE id_usuario = '$id_usr');";
+  $query_tipo_usr = mysqli_query($con,$SQL_tipo_usr);
+  $tipo_usr_array = mysqli_fetch_array($query_tipo_usr);
+  $tipo_usr = $tipo_usr_array[0];
+
+  $SQL_rango = "SELECT rango FROM rango WHERE id_rango IN(SELECT rango FROM formulario WHERE id_form='$id_form');";
+  $query_rango = mysqli_query($con,$SQL_rango);
+  $rango_array = mysqli_fetch_array($query_rango);
+  $rango = $rango_array[0];
+
+
+  if($rango == "Público"){
+    echo true;
+  }elseif ($rango == "Alumnos y profesores") {
+    if(isset($_SESSION['id'])){
+      echo true;
+    }else{
+      echo "0";
+    }
+  }elseif ($rango == "Alumnos") {
+    if($tipo_usr == "Alumno"){
+      echo true;
+    }else{
+      echo "0";
+    }
+  }elseif ($rango == "Profesores") {
+    if($tipo_usr == "Profesor" || $tipo_usr == "Administrador"){
+      echo true;
+    }else{
+      echo "0";
+    }
+  }else{
+    echo "0";
+  }
+?>
